ae384b2aba7899b7307771fc00e34e13d99cc42e
abko27700 <abko27700@gmail.com>
2020-06-16 20:28:31 +0530

    trrr

--- services.php
+++ services.php
@@ -12,7 +12,7 @@
 		<div class="col-md-8 text-center heading-section ftco-animate">
 		<span class="subheading">Services</span>
 		<h2 class="mb-4">Why Choose Us?</h2>
-		<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+		<p>Separith the necessary regelialia. It is a paradisematic country</p>
 		</div>
 	</div>
 	<?php include 'include/tableOfServices.php' ?>
