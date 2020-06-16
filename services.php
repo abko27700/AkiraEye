968a7b68b64bf4ca12ee3ec11def0ca9474ea8c3
abko27700 <abko27700@gmail.com>
2020-06-16 20:36:35 +0530

    doctor name edit

--- services.php
+++ services.php
@@ -9,11 +9,7 @@
 <section class="ftco-section">
 <div class="container">
 	<div class="row justify-content-center mb-5">
-		<div class="col-md-8 text-center heading-section ftco-animate">
-		<span class="subheading">Services</span>
-		<h2 class="mb-4">Why Choose Us?</h2>
-		<p>Separith the necessary regelialia. It is a paradisematic country</p>
-		</div>
+		
 	</div>
 	<?php include 'include/tableOfServices.php' ?>
 </div>
