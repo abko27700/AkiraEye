e7684690a4aa1bf839bb7f2fb70fe0234e45452c
abko27700 <abko27700@gmail.com>
2020-06-16 20:08:28 +0530

    edited dic prof

--- include/tableOfServices.php
+++ include/tableOfServices.php
@@ -32,32 +32,32 @@
 					<div class="col-md-8">
 						<div class="tab-content">
 						  <div class="tab-pane container p-0 active" id="services-1">
-						  	<div class="img" style="background-image: url(images/work-2.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#">General Opthomology</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
 						  <div class="tab-pane container p-0 fade" id="services-2">
-						  	<div class="img" style="background-image: url(images/work-3.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#">Cataract</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
 						  <div class="tab-pane container p-0 fade" id="services-3">
-						  	<div class="img" style="background-image: url(images/work-4.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#">Glaucoma</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
 						  <div class="tab-pane container p-0 fade" id="services-4">
-						  	<div class="img" style="background-image: url(images/work-5.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#">Retina,Vitreous & UVEA</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
 						  <div class="tab-pane container p-0 fade" id="services-5">
-						  	<div class="img" style="background-image: url(images/work-6.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#">Lasik & Refractive Surgery</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
 						  <div class="tab-pane container p-0 fade" id="services-6">
-						  	<div class="img" style="background-image: url(images/work-1.jpg);"></div>
+						  	<div class="img" style="background-image: url(images/sample.jpg);"></div>
 						  	<h3><a href="#"> Orbit & Oculoplasty</a></h3>
 						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						  </div>
